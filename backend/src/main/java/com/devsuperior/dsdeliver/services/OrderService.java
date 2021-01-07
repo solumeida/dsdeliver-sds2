71e877353baa99e281a786ae75e22d421b511a0d
solumeida <solumeida@gmail.com>
2021-01-07 18:31:37 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
 import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
 import com.devsuperior.dsdeliver.repositories.ProductRepository;
@@ -22,10 +24,24 @@ import com.devsuperior.dsdeliver.repositories.ProductRepository;
 public class OrderService {
 	@Autowired //injecto 
 	private OrderRepository repository;//  = new, mais devemos engetar
+	@Autowired //injecto 
+    private ProductRepository productRepository;
 	
 	@Transactional(readOnly = true) // grantir transação, redOnly = true - pra não fazer o lock de escrita no banco!
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWhthProducts();
 		return list.stream().map(x-> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDIG);
+		for(ProductDTO p: dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order= repository.save(order);
+		return new OrderDTO(order);
+	}	
 }
+ 
